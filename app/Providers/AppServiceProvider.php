41a64b1e853b09bed99b36c8d2181621c2a367a1
Wildan Maulana <wildanmh515@gmail.com>
2026-08-20 21:25:20 +0700

    fix fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Memaksa HTTPS jika sedang online (production)
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
     }
-}use
+}
